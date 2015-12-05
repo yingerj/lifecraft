571414496192004373a803cf970ed33e1624b068
Jack Zark Yinger <jack.yinger@gmail.com>
2015-12-05 10:43:37 -0800

    Added game grid array and got the checker board working with it.

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,36 @@ extern crate graphics;
 use piston_window::*;
 use rand::os::OsRng;
 
-static grid_size: u32 = 10;
-static cell_size: u32 = 10;
+const grid_size: usize = 10;
+const cell_size: usize = 10;
+
 
 fn main() {
+    // Set up our game board
+    let mut grid: [[u8; grid_size]; grid_size] = [[0; grid_size]; grid_size];
+
     // Set up our window, duh
     let window: PistonWindow =
-        WindowSettings::new("Hello Piston!", [grid_size * cell_size; 2])
+        WindowSettings::new("Hello Piston!", [(grid_size * cell_size) as u32; 2])
         .exit_on_esc(true).build().unwrap();
+
+    // Our checkerboard initial values
+    let mut fill: bool = true;
+    for x in 0..grid_size {
+        for y in 0..grid_size {
+            fill = !fill;
+            println!("fill: {}", fill);
+            if fill {
+                grid[x][y] = fill as u8;
+            }
+        }
+        if grid_size % 2 == 0 {
+            fill = !fill;
+        }
+    }
+
     // For each window event e:
     for e in window {
-        // c is ?, g is ?
-        let mut fill: bool = true;
         e.draw_2d(|c, g| {
             println!("Redraw");
             clear([1.0; 4], g);
@@ -28,9 +46,8 @@ fn main() {
                     let y_min: f64 = (y * cell_size) as f64;
                     let y_max: f64 = y_min as f64 + (cell_size as f64);
                     println!("   y min: {} max: {}", y_min, y_max);
-                    fill = !fill;
                     println!("fill: {}", fill);
-                    if fill {
+                    if grid[x][y] > 0 {
                         rectangle([1.0, 0.0, 0.0, 1.0], // red
                                   [x_min, y_min, cell_size as f64, cell_size as f64],
                                   c.transform, g);
