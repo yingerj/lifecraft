658dd41941aeaf033d0c473af7bca8b139b76f28
Jack Zark Yinger <jack.yinger@gmail.com>
2015-12-05 16:03:29 -0800

    Added functionality to toggle cells in game grid.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use piston_window::*;
 //use rand::os::OsRng;
 
 const GRID_SIZE: usize = 10; //cells
-const CELL_SIZE: usize = 10; //pixels
+const CELL_SIZE: usize = 40; //pixels
 
 fn checkerboard(grid: &mut [[u8; GRID_SIZE]; GRID_SIZE]) {
     let mut fill: bool = true;
@@ -22,10 +22,27 @@ fn checkerboard(grid: &mut [[u8; GRID_SIZE]; GRID_SIZE]) {
     }
 }
 
+
+fn toggle_cell(grid: &mut [[u8; GRID_SIZE]; GRID_SIZE], cell_pos: [usize; 2]) {
+    let cell = &mut grid[cell_pos[0]][cell_pos[1]];
+    if *cell > 0 {
+        *cell = 0;
+    }
+    else {
+        *cell = 1;
+    }
+}
+
+fn window_to_board_coordinates(screen_pos: [f64; 2]) -> [usize; 2] {
+    let cell_size = CELL_SIZE as f64;
+    let cell_float = [screen_pos[0]/cell_size, screen_pos[1]/cell_size];
+    [cell_float[0] as usize, cell_float[1] as usize]
+}
+
 fn main() {
     // Place to store latest mouse possition from mouse event
-    let mut mouse_x: f64 = 0.0;
-    let mut mouse_y: f64 = 0.0;
+    let mut mouse_pos: [f64; 2] = [0.0, 0.0];
+
     // Set up our game board
     let mut grid: [[u8; GRID_SIZE]; GRID_SIZE] = [[0; GRID_SIZE]; GRID_SIZE];
 
@@ -46,7 +63,7 @@ fn main() {
                     if grid[x][y] > 0 {
                         let x_position: f64 = (x * CELL_SIZE) as f64;
                         let y_position: f64 = (y * CELL_SIZE) as f64;
-                        rectangle([1.0, 0.0, 0.0, 1.0], // red
+                        rectangle([0.0, 0.0, 0.0, 1.0], // red
                                   [x_position, y_position, CELL_SIZE as f64, CELL_SIZE as f64],
                                   c.transform, g);
                     }
@@ -55,14 +72,14 @@ fn main() {
         });
 
         if let Some(pos) = e.mouse_cursor_args() {
-            mouse_x = pos[0];
-            mouse_y = pos[1];
+            mouse_pos = pos;
         }
 
         if let Some(button) = e.press_args() {
             if button == Button::Mouse(MouseButton::Left) {
                 println!("Pressed {:?}", button);
-                println!("Mouse position x:{:?} y:{:?}", mouse_x, mouse_y);
+                println!("Mouse position x:{:?} y:{:?}", mouse_pos[0], mouse_pos[1]);
+                toggle_cell(&mut grid, window_to_board_coordinates(mouse_pos));
             }
         }
     }
